281c4dcad959d84d89a6148a2a015a14eb94f457
Harsh Kothari <harsh.kothari@hyperverge.co>
2021-01-21 22:39:13 +0530

    TS variable type updated in BusController

--- src/controllers/BusController.ts
+++ src/controllers/BusController.ts
@@ -5,14 +5,16 @@ const common_util = require('../utils/common_utils');
 exports.createBus = async (req: Request, res: Response, next: NextFunction) => {
   const bus = await db_util.createBus(req.body, req.body.no_of_seats);
   common_util.sendResponse(res, bus);
+  return next();
 };
 
 exports.findAllBuses = async (req: Request, res: Response, next: NextFunction) => {
-  const buses = await db_util.getBuses(req.params);
+  const buses = await db_util.getBuses(req.query);
   common_util.sendResponse(res, buses);
+  return next();
 };
 
 exports.getBusInfo = async (req: Request, res: Response, next: NextFunction) => {
-  const bus = await db_util.getBus(req.params.busId);
+  const bus = await db_util.getBusInfo(req.params.busId);
   common_util.sendResponse(res, bus);
 };
