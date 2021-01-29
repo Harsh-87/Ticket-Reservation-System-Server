362b12851317eccc7006c022c3967eee8726beef
Pratik Gupta <pratik17100@iiitnr.edu.in>
2021-01-29 17:36:36 +0530

    setup cors done for port 3000 react app

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,13 @@ connect.then(
 );
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
